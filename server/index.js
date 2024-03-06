f42dafb63cc43614053b1204fac190d0de87f0b3
afreedituttu <afreedisulfiker@gmail.com>
2024-03-06 18:27:18 +0530

    stable version using context

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const errorHandler = require('./middlewares/errorHandler')
+const logger = require('morgan');
 
 const dotenv = require('dotenv');
 dotenv.config({
@@ -14,6 +15,7 @@ const routes = require('./routes')
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
+app.use(logger('dev'))
 app.use('/api', routes)
 app.get('/test',(req, res)=>{
     return res.status(200).json({"message":"working"});
