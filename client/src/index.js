01bef6086617a44b179a2ad20c023b6ed7cf7afe
afreedituttu <afreedisulfiker@gmail.com>
2024-03-08 11:41:30 +0530

    store provided, stable. now redefining logics at component level

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import ContextProvider from './context/context';
 import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux'
+import store from './features/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <ContextProvider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Provider>
   </ContextProvider>
 );
